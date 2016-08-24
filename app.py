446eebd5b8ccab03a73dd1629ab6f95eb1a47ae5
olafurj <olafurjohannss@gmail.com>
2016-08-24 14:12:50 +0000

    application and ignore file

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import JSON
+from models.user import User
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-app = Flask('olafurjohannsson')
+
 
 @app.route('/')
 def index():
+    admin = User.query.filter_by(username='admin').first()
+    print(admin)
     return render_template('index.html')
+
+
+
+@app.route('/blog')
+def blog():
+    pass
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+    db.create_all()
+
+    admin = User('admin', 'olafurjohannss@gmail.com')
+    db.session.add(admin)
+    db.session.commit()
